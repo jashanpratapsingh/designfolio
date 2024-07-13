fe2f6427d22f0cc35386a01b43c873b97dc0c521
jashanpratapsingh <jashanpratap123@gmail.com>
2024-07-13 14:36:07 +0400

    correcting the paths in the file

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const ejs = require("ejs");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
@@ -42,116 +43,116 @@ app.get("/inter-school", function(req, res) {
 });
 
 app.get("/images/jashan.jpeg", function(req, res) {
-    res.sendFile(__dirname + "/images/jashan.jpeg")
+    res.sendFile(__dirname + "/../images/jashan.jpeg")
 });
 
 app.get("/images/elonbiography.jpg", function(req, res) {
-    res.sendFile(__dirname + "/images/elonbiography.jpg")
+    res.sendFile(__dirname + "/../images/elonbiography.jpg")
 });
 
 app.get("/images/faultinourstars.jpg", function(req, res) {
-    res.sendFile(__dirname + "/images/fault.jpg")
+    res.sendFile(__dirname + "/../images/fault.jpg")
 });
 
 app.get("/images/artofwar.jpg", function(req, res) {
-    res.sendFile(__dirname + "/images/artofwar.jpg")
+    res.sendFile(__dirname + "/../images/artofwar.jpg")
 });
 
 app.get("/images/timeline1.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline1.svg")
+    res.sendFile(__dirname + "/../images/timeline1.svg")
 });
 
 app.get("/public/css/styles.css", function(req, res) {
-    res.sendFile(__dirname + "/public/css/styles.css")
+    res.sendFile(__dirname + "/../public/css/styles.css")
 });
 
 app.get("/images/timeline2.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline2.svg")
+    res.sendFile(__dirname + "/../images/timeline2.svg")
 });
 
 app.get("/images/timeline3.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline3.svg")
+    res.sendFile(__dirname + "/../images/timeline3.svg")
 });
 
 app.get("/images/timeline4.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline4.svg")
+    res.sendFile(__dirname + "/../images/timeline4.svg")
 });
 
 app.get("/images/timeline5.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline5.svg")
+    res.sendFile(__dirname + "/../images/timeline5.svg")
 });
 
 app.get("/images/timeline6.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline6.svg")
+    res.sendFile(__dirname + "/../images/timeline6.svg")
 });
 
 app.get("/images/timeline7.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline7.svg")
+    res.sendFile(__dirname + "/../images/timeline7.svg")
 });
 
 app.get("/images/timeline8.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline8.svg")
+    res.sendFile(__dirname + "/../images/timeline8.svg")
 });
 
 app.get("/images/timeline9.svg", function(req, res) {
-    res.sendFile(__dirname + "/images/timeline9.svg")
+    res.sendFile(__dirname + "/../images/timeline9.svg")
 });
 
 app.get("/images/weaknesses.png", function(req, res) {
-    res.sendFile(__dirname + "/images/weaknesses.png")
+    res.sendFile(__dirname + "/../images/weaknesses.png")
 });
 
 app.get("/images/publicate.jpeg", function(req, res) {
-    res.sendFile(__dirname + "/images/publicate.jpeg")
+    res.sendFile(__dirname + "/../images/publicate.jpeg")
 });
 
 app.get("/images/publicate1.png", function(req, res) {
-    res.sendFile(__dirname + "/images/publicate1.png")
+    res.sendFile(__dirname + "/../images/publicate1.png")
 });
 
 app.get("/images/publicate2.png", function(req, res) {
-    res.sendFile(__dirname + "/images/publicate2.png")
+    res.sendFile(__dirname + "/../images/publicate2.png")
 });
 
 app.get("/images/publicate3.png", function(req, res) {
-    res.sendFile(__dirname + "/images/publicate3.png")
+    res.sendFile(__dirname + "/../images/publicate3.png")
 });
 
 
 app.get("/videos/podcast.mp4", function(req, res) {
-    res.sendFile(__dirname + "/videos/podcast.mp4")
+    res.sendFile(__dirname + "/../videos/podcast.mp4")
 });
 
 app.get("/videos/publicate.mp4", function(req, res) {
-    res.sendFile(__dirname + "/videos/publicate.mp4")
+    res.sendFile(__dirname + "/../videos/publicate.mp4")
 });
 
 app.get("/images/favicon-16x16.png", function(req, res) {
-    res.sendFile(__dirname + "/images/png/favicon-16x16.png")
+    res.sendFile(__dirname + "/../images/png/favicon-16x16.png")
 });
 
 app.get("/images/favicon-96x96.png", function(req, res) {
-    res.sendFile(__dirname + "/images/png/favicon-96x96.png")
+    res.sendFile(__dirname + "/../images/png/favicon-96x96.png")
 });
 
 app.get("/images/favicon-32x32.png", function(req, res) {
-    res.sendFile(__dirname + "/images/png/favicon-32x32.png")
+    res.sendFile(__dirname + "/../images/png/favicon-32x32.png")
 });
 
 app.get("/images/codewithme.png", function(req, res) {
-    res.sendFile(__dirname + "/images/png/codewithme.png")
+    res.sendFile(__dirname + "/../images/png/codewithme.png")
 });
 
 app.get("/images/favicon/favicon.ico", function(req, res) {
-    res.sendFile(__dirname + "/images/favicon/favicon.ico")
+    res.sendFile(__dirname + "/../images/favicon/favicon.ico")
 });
 
 app.get("/sitemap.xml", function(req, res) {
-    res.sendFile(__dirname + "/sitemap.xml")
+    res.sendFile(__dirname + "/../sitemap.xml")
 });
 
 app.get("/scripts/main.js", function(req, res) {
-    res.sendFile(__dirname + "/public/javascript/main.js")
+    res.sendFile(__dirname + "/../public/javascript/main.js")
 });
 
 app.listen(process.env.PORT || 3000, function() {
