e30cbed9a91ddd8b389dc637f78fc69632278733
jashanpratapsingh <jashanpratap123@gmail.com>
2024-07-13 15:27:53 +0400

    making the website static and serverless

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     res.render("index")
